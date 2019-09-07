0cb18f5774fd5c4b65accd62e881b7d387a20c2f
Kelvyn Tomaz <kelvyntomaz@gmail.com>
2019-09-06 23:18:56 -0300

    Corrigindo again

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -12,7 +12,8 @@ resource "aws_instance" "ec2-aws" {
   ami           = "ami-00eb20669e0990cb4"
   instance_type = "t2.micro"
   key_name = "Projeto-Yuri.pem"
-  tags {
+
+  tags = {
     Name = "Impacta - Kelvyn e Michell"
   }
 }
